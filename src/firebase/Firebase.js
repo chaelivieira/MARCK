c32bd1640d16b8fc2bed0f2262cabd30601552b4
msgyankee <msgyankee@gmail.com>
2020-12-06 16:16:51 -0500

    Firebase config

---
+++ src/firebase/Firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+
+const firebaseApp = firebase.initializeApp({
+    apiKey: "AIzaSyCTw6_VvH586DDXd2kF5LtUQCwCKdIPit8",
+    authDomain: "unwrapped-d15ef.firebaseapp.com",
+    projectId: "unwrapped-d15ef",
+    storageBucket: "unwrapped-d15ef.appspot.com",
+    messagingSenderId: "1065816994881",
+    appId: "1:1065816994881:web:cc814964319a907b74c2c0"
+});
+
+export default firebaseApp;
