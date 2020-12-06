d92b4c776c2a27a13584216f91586598373135fb
msgyankee <msgyankee@gmail.com>
2020-12-06 17:24:08 -0500

    Less warnings & logs

--- src/components/Login.js
+++ src/components/Login.js
@@ -12,7 +12,7 @@ function Login(){
 
     window.addEventListener("message", async (event) =>{
         if(event.origin !== "http://localhost:9000") {console.log("bad origin"); return;}
-        let login = await firebase.auth().signInWithCustomToken(event.data);
+        await firebase.auth().signInWithCustomToken(event.data);
     });
 
     function onSignInButtonClick() {
