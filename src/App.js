434233911839d02149642eabaea06696d769b8ea
Kirsten Meidlinger <kirstenalice426@gmail.com>
2020-12-08 19:56:59 -0500

    login/logout navlink changes on log in/log out

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import { AuthProvider } from "./firebase/Auth";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React, { useContext, useEffect, useState } from "react";
+import { AuthProvider, AuthContext } from "./firebase/Auth";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 import { NavLink } from "react-router-dom";
 import LoginStatus from "./components/LoginStatus";
+import Home from "./components/Home";
 import Login from "./components/Login";
 import Logout from "./components/Logout";
 import "./App.css";
+import userEvent from "@testing-library/user-event";
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(null);
+
   return (
     <AuthProvider>
       <Router>
         <div className="App">
           <header className="App-header">
-            <NavLink to="/login">Login</NavLink>
+            <h1>Unwrapped</h1>
+            <nav>
+              <NavLink to="/playlists">Playlists</NavLink>
+              <NavLink to="/stats">Stats</NavLink>
+              {/** I'd like this to say log in/out based on loginstatus, how to get this to rerender */}
+              {currentUser === null ? (
+                <NavLink to="/login">Log in</NavLink>
+              ) : (
+                <NavLink to="/logout">Log out</NavLink>
+              )}
+            </nav>
           </header>
           <div className="App-body">
-            <LoginStatus />
+            <LoginStatus updateUser={setCurrentUser} />
             <br />
             <Logout />
+            <Route exact path="/">
+              <Redirect to="/stats" />
+            </Route>
+            {/** change component when Stats exists  */}
+            <Route path="/stats" component={Home} />
+            {/** change component when Playlists exists  */}
+            <Route path="/playlists" component={Home} />
             <Route path="/login" component={Login} />
             <Route path="/logout" component={Logout} />
           </div>
---
+++ src/components/Home.js
@@ -0,0 +1,8 @@
+import React from "react";
+import firebase from "firebase/app";
+
+function Home() {
+  return <div>Welcome to Unwrapped!</div>;
+}
+
+export default Home;
