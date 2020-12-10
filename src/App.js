bd0c529e9e0668b0bf6445088f3c54ecc640e4a5
msgyankee <msgyankee@gmail.com>
2020-12-10 14:31:33 -0500

    resolve header

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState } from "react";
 import { AuthProvider } from "./firebase/Auth";
 import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
@@ -8,18 +7,6 @@ import Logout from "./components/Logout";
 import Playlists from "./components/Playlists";
 import Stats from "./components/Stats";
 import "./App.css";
-=======
-import { AuthProvider } from './firebase/Auth';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { NavLink } from 'react-router-dom';
-import LoginStatus from './components/LoginStatus';
-import Login from './components/Login';
-import Logout from './components/Logout';
-import Home from './components/Home';
-import Stats from './components/Stats';
-import Playlists from './components/Playlists';
-import './App.css';
->>>>>>> main
 
 function App() {
   const [currentUser, setCurrentUser] = useState(null);
@@ -33,34 +20,6 @@ function App() {
       <Router>
         <div className="App">
           <header className="App-header">
-<<<<<<< HEAD
-            <h1 className="Header-title">Unwrapped</h1>
-            <nav>
-              <NavLink to="/playlists">Playlists</NavLink>
-              <NavLink to="/stats">Stats</NavLink>
-              {currentUser === null ? (
-                <NavLink to="/login">Log in</NavLink>
-              ) : (
-                <NavLink to="/logout">Log out</NavLink>
-              )}
-            </nav>
-          </header>
-          <div className="App-body">
-            <br />
-            <Route exact path="/">
-              <Redirect to="/stats" />
-            </Route>
-            <Route path="/stats" component={Stats} />
-            <Route path="/playlists">
-              <Playlists />
-            </Route>
-            <Route path="/login">
-              <Login onLogIn={receiveUserData} />
-            </Route>
-            <Route path="/logout">
-              <Logout onLogOut={receiveUserData} />
-            </Route>
-=======
             <LoginStatus/>
             <NavLink to="/login">Login</NavLink>
             <NavLink to="/playlists">Playlists</NavLink>
@@ -73,7 +32,6 @@ function App() {
             <Route path="/logout" component={Logout}/>
             <Route path="/playlists" component={Playlists}/>
             <Route path="/stats" component={Stats}/>
->>>>>>> main
           </div>
         </div>
       </Router>
