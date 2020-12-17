13c964f4aa3c7fb70697741c3706105a84df7e07
Kirsten Meidlinger <kirstenalice426@gmail.com>
2020-12-17 11:43:58 -0500

    update Stats.js pdf button styling

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -33,6 +33,12 @@ const useStyles = makeStyles({
     "&:hover": {
       backgroundColor: "#c3096b",
     },
+    "&:focus": {
+      backgroundColor: "#c3096b",
+    },
+    "&:active": {
+      backgroundColor: "#c3096b",
+    },
   },
 });
 
@@ -112,7 +118,7 @@ function Stats() {
               Disclaimer: If your Top Artists or Top Songs of All Time are
               empty, nothing will show in that field on the PDF
             </Textfit>
-            <div id="#foo"></div>
+            <div id="foo"></div>
           </div>
           <h2 className={classes.h2}>Top Songs:</h2>
           <Songs />
